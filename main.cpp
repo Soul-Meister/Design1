216f5a3bbe5cec54959db2b46825eca0cd3467ef
Edward <eddie.buckley9@gmail.com>
2026-03-19 16:21:22 -0600

    random stuff

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #import <iostream>
 
-
+using namespace std;
 int main() {
-    std::cout << "Hello world!";
-    return 0;
+    cout << "Hello world!\n";
+int var = 1;
+switch (var) {
+    case 0:
+        cout << "its zero yo";
+        break;
+        case 1:
+        cout << "its one yo";
+        break;
+    default:
+        cout << "its another yo";
+        break;
+}
 
+
+
+
+    return 0;
 }
     //
 // Created by edward on 3/17/26.
